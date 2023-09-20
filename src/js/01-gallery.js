73398ce8c747624659ab0c245d07d42a80442241
Uliana Nadorozhna <unadorozhna1@gmail.com>
2023-09-20 16:42:06 +0300

    1 task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,27 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
+import SimpleLightbox from "simplelightbox";
+
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 console.log(galleryItems);
+
+
+const gallery = document.querySelector(".gallery");
+
+const imageHtml = galleryItems.map((img) => { 
+    return `<li class="gallery__item">
+                <a class="gallery__link" href="${img.original}">
+                    <img class="gallery__image" src="${img.preview}" alt="${img.description}" />
+                </a>
+            </li>
+        `}).join("");
+
+gallery.insertAdjacentHTML("afterBegin", imageHtml);
+
+
+new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+ });
